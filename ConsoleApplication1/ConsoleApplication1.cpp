a0a0191320b64b384a755411db43bf485ad00333
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-03-26 11:02:12 +0700

    membuat system cls

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,29 +3,46 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
-int penjumlahan(int a, int b) {
+int penjumlahan(int a, int b)
+{
     return a + b;
 }
 
-int pengurangan(int a, int b) {
+int pengurangan(int a, int b)
+{
     return a - b;
 }
 
-int perkalian(int a, int b) {
+int perkalian(int a, int b) 
+{
     return a * b;
 }
 
-float pembagian(float a, float b) {
+float pembagian(float a, float b) 
+{
     return a / b;
 }
 
-void inputdata() {
+void inputdata()
+{
     cout << "\nMasukkan bilangan pertama :";
     cin >> bilangan1;
     cout << "\nMasukkan bilangan kedua :";
     cin >> bilangan2;
 }
 
-int main(){
-    
+int main() 
+{
+    int pilihan;
+    do
+    {
+        system("cls");
+        cout << "Menu Kalkulator Sederhana" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cin >> pilihan;     
+    }
 }
