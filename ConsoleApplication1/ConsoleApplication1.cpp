187d908e41e126018f8a6eab29df2c4fad8f481a
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2024-03-26 11:20:11 +0700

    revisi error

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -43,6 +43,7 @@ int main()
         cout << "3. Perkalian" << endl;
         cout << "4. Pembagian" << endl;
         cout << "5. Keluar" << endl;
+        cout << "Masukkan pilihan : ";
         cin >> pilihan; 
 
         switch (pilihan)
